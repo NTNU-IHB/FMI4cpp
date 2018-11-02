4a3677726577642673ace8d0b47ca987110f52fb
Lars Ivar Hatledal <laht@hials.no>
2018-11-02 23:12:52 +0100

    add to_string(Status)

--- include/fmi4cpp/Status.hpp
+++ include/fmi4cpp/Status.hpp
@@ -1,9 +1,31 @@
-//
-// Created by LarsIvar on 02.11.2018.
-//
+/*
+ * The MIT License
+ *
+ * Copyright 2017-2018 Norwegian University of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #ifndef FMI4CPP_STATUS_HPP
 #define FMI4CPP_STATUS_HPP
+
+#include <string>
 
 namespace fmi4cpp {
 
@@ -15,6 +37,23 @@ namespace fmi4cpp {
         Fatal,
         Pending
     };
+
+    std::string to_string(Status status) {
+        switch (status) {
+            case Status::OK:
+                return "OK";
+            case Status::Warning:
+                return "Warning";
+            case Status::Discard:
+                return "Discard";
+            case Status::Error:
+                return "Error";
+            case Status::Fatal:
+                return "Fatal";
+            case Status::Pending:
+                return "Pending";
+        }
+    }
 
 }
 
