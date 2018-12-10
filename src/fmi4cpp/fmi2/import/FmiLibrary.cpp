e7ff53da7b53a4325d244ba2d8e36124a2f8e5f5
Lars Ivar Hatledal <laht@hials.no>
2018-12-10 10:53:07 +0100

    fix typo

--- src/fmi4cpp/fmi2/import/FmiLibrary.cpp
+++ src/fmi4cpp/fmi2/import/FmiLibrary.cpp
@@ -152,7 +152,7 @@ fmi2Component FmiLibrary::instantiate(const std::string instanceName, const fmi2
                                        resourceLocation.c_str(), &callback, visible, loggingOn);
 
     if (c == nullptr) {
-        throw std::runtime_error("Fatal: fmi2Instantiate returned nullpptr, unable to instantiate FMU instance!");
+        throw std::runtime_error("Fatal: fmi2Instantiate returned nullptr, unable to instantiate FMU instance!");
     }
 
     return c;
