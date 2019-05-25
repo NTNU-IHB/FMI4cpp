886b42f7da52950dcb8745e3d418e09cad6b378f
Lars Ivar Hatledal <laht@ntnu.no>
2019-05-25 13:21:10 +0200

    link openssl on unix if available

--- cmake/FindCURL_.cmake
+++ cmake/FindCURL_.cmake
@@ -1,37 +1,36 @@
 
 find_package(CURL REQUIRED)
+find_package(OpenSSL QUIET COMPONENTS Crypto SSL )
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL_
-        REQUIRED_VARS CURL_INCLUDE_DIR CURL_LIBRARY)
+if(NOT TARGET CURL::libcurl)
+    add_library(CURL::libcurl UNKNOWN IMPORTED)
+    set_target_properties(CURL::libcurl PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
 
-if (CURL_FOUND)
-
-    if(NOT TARGET CURL::libcurl)
-        add_library(CURL::libcurl UNKNOWN IMPORTED)
+    if(EXISTS "${CURL_LIBRARY}")
         set_target_properties(CURL::libcurl PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
-
-        if(EXISTS "${CURL_LIBRARY}")
-            set_target_properties(CURL::libcurl PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                    IMPORTED_LOCATION "${CURL_LIBRARY}")
-        endif()
-        if(CURL_LIBRARY_RELEASE)
-            set_property(TARGET CURL::libcurl APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS RELEASE)
-            set_target_properties(CURL::libcurl PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                    IMPORTED_LOCATION_RELEASE "${CURL_LIBRARY_RELEASE}")
-        endif()
-        if(CURL_LIBRARY_DEBUG)
-            set_property(TARGET CURL::libcurl APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS DEBUG)
-            set_target_properties(CURL::libcurl PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                    IMPORTED_LOCATION_DEBUG "${CURL_LIBRARY_DEBUG}")
-        endif()
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                IMPORTED_LOCATION "${CURL_LIBRARY}")
     endif()
+    if(CURL_LIBRARY_RELEASE)
+        set_property(TARGET CURL::libcurl APPEND PROPERTY
+                IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(CURL::libcurl PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                IMPORTED_LOCATION_RELEASE "${CURL_LIBRARY_RELEASE}")
+    endif()
+    if(CURL_LIBRARY_DEBUG)
+        set_property(TARGET CURL::libcurl APPEND PROPERTY
+                IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(CURL::libcurl PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                IMPORTED_LOCATION_DEBUG "${CURL_LIBRARY_DEBUG}")
+    endif()
+endif()
 
+if (UNIX AND OpenSSL_FOUND)
+    set(INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
 
+    set_property(TARGET CURL::libcurl APPEND PROPERTY
+            INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
 endif()
