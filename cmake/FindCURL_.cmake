683e8393e7d102ebe73899091e367cd02e226ef4
Lars Ivar Hatledal <laht@ntnu.no>
2019-05-25 13:14:05 +0200

    update custom find curl

--- cmake/FindCURL_.cmake
+++ cmake/FindCURL_.cmake
@@ -1,6 +1,5 @@
 
 find_package(CURL REQUIRED)
-find_package(OpenSSL COMPONENTS Crypto SSL)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL_
@@ -8,21 +7,31 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL_
 
 if (CURL_FOUND)
 
-    if (NOT TARGET curl::curl)
-        add_library(curl::curl UNKNOWN IMPORTED)
-        set_property(TARGET curl::curl
-                APPEND
-                PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIR}")
-        set_property(TARGET curl::curl
-                APPEND
-                PROPERTY IMPORTED_LOCATION "${CURL_LIBRARY}")
-        if (OpenSSL_FOUND)
-            set(INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
+    if(NOT TARGET CURL::libcurl)
+        add_library(CURL::libcurl UNKNOWN IMPORTED)
+        set_target_properties(CURL::libcurl PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
+
+        if(EXISTS "${CURL_LIBRARY}")
+            set_target_properties(CURL::libcurl PROPERTIES
+                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                    IMPORTED_LOCATION "${CURL_LIBRARY}")
         endif()
-        set_property(TARGET curl::curl
-                APPEND
-                PROPERTY INTERFACE_LINK_LIBRARIES
-                ${INTERFACE_LINK_LIBRARIES})
+        if(CURL_LIBRARY_RELEASE)
+            set_property(TARGET CURL::libcurl APPEND PROPERTY
+                    IMPORTED_CONFIGURATIONS RELEASE)
+            set_target_properties(CURL::libcurl PROPERTIES
+                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                    IMPORTED_LOCATION_RELEASE "${CURL_LIBRARY_RELEASE}")
+        endif()
+        if(CURL_LIBRARY_DEBUG)
+            set_property(TARGET CURL::libcurl APPEND PROPERTY
+                    IMPORTED_CONFIGURATIONS DEBUG)
+            set_target_properties(CURL::libcurl PROPERTIES
+                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                    IMPORTED_LOCATION_DEBUG "${CURL_LIBRARY_DEBUG}")
+        endif()
     endif()
+
 
 endif()
