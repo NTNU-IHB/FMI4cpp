d23cca2c01d8f50f16c30e9dd15391b85ff73963
laht <laht@ntnu.no>
2019-09-02 15:03:15 +0200

    Make run_conan_install.sh cross-platform

--- run_conan_install.sh
+++ run_conan_install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug -o curl=True
-conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release -o curl=True
+conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-conan" --f
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+
+    MINGW*)     conan install . -s build_type=Debug --install-folder=cmake-build-debug -o curl=True;;
+    MINGW*)     conan install . -s build_type=Release --install-folder=cmake-build-release -o curl=True;;
+    Linux*)     conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug -o curl=True;;
+    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release -o curl=True;;
+esac
