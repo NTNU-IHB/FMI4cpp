e6ded7f60576e4cb2146ab35aa35d34b86ce64b9
laht <laht@ntnu.no>
2019-09-02 15:56:26 +0200

    Fix conan install script

--- run_conan_install.sh
+++ run_conan_install.sh
@@ -5,8 +5,8 @@ conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-c
 unameOut="$(uname -s)"
 case "${unameOut}" in
 
-    MINGW*)     conan install . -s build_type=Debug --install-folder=cmake-build-debug;;
-    MINGW*)     conan install . -s build_type=Release --install-folder=cmake-build-release;;
-    Linux*)     conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug;;
-    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release;;
+    MINGW*)     conan install . -s build_type=Debug --install-folder=cmake-build-debug
+                conan install . -s build_type=Release --install-folder=cmake-build-release;;
+    Linux*)     conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug
+                conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release;;
 esac
