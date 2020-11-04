f841fdbb382b0a38b5128b562c2c9fefcb5fea1f
Lars Ivar Hatledal <laht@hials.no>
2020-11-04 09:12:55 +0100

    cleanup

--- examples/fmu_test.cpp
+++ examples/fmu_test.cpp
@@ -1,27 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2017-2018 Norwegian University of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 #include <fmi4cpp/fmi4cpp.hpp>
 
 #include <iostream>
@@ -61,7 +37,7 @@ int main()
     std::vector<fmi2ValueReference> vr = {md->get_variable_by_name("Temperature_Reference").value_reference,
         md->get_variable_by_name("Temperature_Room").value_reference};
 
-    double t = 0;
+    double t;
     while ((t = slave1->get_simulation_time()) <= stop) {
 
         if (!slave1->step(stepSize)) { break; }
