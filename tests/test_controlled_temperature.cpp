31cd95965da94652597e340c4b1b78c93fb830ec
Lars Ivar Hatledal <laht@ntnu.no>
2022-04-11 11:43:20 +0200

    Use catch2 for testing (#130)

--- tests/test_controlled_temperature.cpp
+++ tests/test_controlled_temperature.cpp
@@ -1,9 +1,8 @@
 
-#define BOOST_TEST_MODULE ControlledTemperature_test1
-
 #include <fmi4cpp/fmi2/fmi2.hpp>
 
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 #include <string>
 
@@ -12,11 +11,10 @@ using namespace fmi4cpp;
 const double step_size = 1E-4;
 const fmi2ValueReference vr = 46;
 
-BOOST_AUTO_TEST_CASE(ControlledTemperature_test1)
+TEST_CASE("ControlledTemperature_test1")
 {
-
     const std::string fmu_path = "../resources/fmus/2.0/cs/20sim/4.6.4.8004/"
-                            "ControlledTemperature/ControlledTemperature.fmu";
+                                 "ControlledTemperature/ControlledTemperature.fmu";
 
     auto fmu = fmi2::fmu(fmu_path).as_cs_fmu();
 
@@ -26,18 +24,18 @@ BOOST_AUTO_TEST_CASE(ControlledTemperature_test1)
             numOutputs++;
         }
     }
-    BOOST_CHECK_EQUAL(2, numOutputs);
+    CHECK(2 == numOutputs);
 
     auto slave = fmu->new_instance();
-    BOOST_CHECK(slave->setup_experiment());
-    BOOST_CHECK(slave->enter_initialization_mode());
-    BOOST_CHECK(slave->exit_initialization_mode());
+    CHECK(slave->setup_experiment());
+    CHECK(slave->enter_initialization_mode());
+    CHECK(slave->exit_initialization_mode());
 
     double ref;
-    BOOST_CHECK(slave->step(step_size));
-    BOOST_CHECK(slave->read_real(vr, ref));
+    CHECK(slave->step(step_size));
+    CHECK(slave->read_real(vr, ref));
 
-    BOOST_CHECK_EQUAL(298.15, ref);
+    CHECK(298.15 == Approx(ref));
 
-    BOOST_CHECK(slave->terminate());
+    CHECK(slave->terminate());
 }
--- tests/test_modeldescription2.cpp
+++ tests/test_modeldescription2.cpp
@@ -1,32 +1,31 @@
 
-#define BOOST_TEST_MODULE FmuExportCrossCompile_test1
-
 #include <fmi4cpp/fmi2/fmi2.hpp>
 
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 #include <string>
 #include <vector>
 
 using namespace fmi4cpp::fmi2;
 
-const std::string fmu_path = "../resources/fmus/2.0/cs/OpenModelica/v1.11.0/"
-                        "FmuExportCrossCompile/modelDescription.xml";
-
-BOOST_AUTO_TEST_CASE(FmuExportCrossCompile_test1)
+TEST_CASE("FmuExportCrossCompile_test1")
 {
+
+    const std::string fmu_path = "../resources/fmus/2.0/cs/OpenModelica/v1.11.0/"
+                                 "FmuExportCrossCompile/modelDescription.xml";
 
     auto md = parse_model_description(fmu_path);
 
-    BOOST_CHECK_EQUAL("structured", md->variable_naming_convention.value());
+    CHECK("structured" == md->variable_naming_convention.value());
 
     std::vector<unknown> derivatives = md->model_structure->derivatives;
-    BOOST_CHECK_EQUAL(2, derivatives.size());
+    CHECK(2 == derivatives.size());
 
-    BOOST_CHECK_EQUAL(3, derivatives[0].index);
-    BOOST_CHECK_EQUAL(4, derivatives[1].index);
+    CHECK(3 == derivatives[0].index);
+    CHECK(4 == derivatives[1].index);
 
-    BOOST_CHECK_EQUAL(true, derivatives[0].dependencies.has_value());
-    BOOST_CHECK_EQUAL(2, derivatives[0].dependencies.value()[0]);
-    BOOST_CHECK_EQUAL("dependent", derivatives[0].dependencies_kind.value()[0]);
+    CHECK(true == derivatives[0].dependencies.has_value());
+    CHECK(2 == derivatives[0].dependencies.value()[0]);
+    CHECK("dependent" == derivatives[0].dependencies_kind.value()[0]);
 }
