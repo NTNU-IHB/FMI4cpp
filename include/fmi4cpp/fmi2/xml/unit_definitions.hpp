23191233c51b74cec59633ee3340d16b2f69d697
WeilongWen <81895406+WeilongWen@users.noreply.github.com>
2025-02-06 18:31:25 +0800

    Provides unit structure definitions and supports XML content parsing for these units. (#156)
    Co-authored-by: Wenweil1 <81895406+Wenweil1@users.noreply.github.com>

---
+++ include/fmi4cpp/fmi2/xml/unit_definitions.hpp
@@ -0,0 +1,54 @@
+/*
+ * File Name:    unit_definitions.h
+ * Description:  Defines structures for handling physical units and their conversions, including base units, display units, and unit definitions.
+ * Author:       weilong.wen
+ * Email:       weilong.wen.mail@gmail.com
+ * Created:     2025-02-03
+ */
+
+#ifndef FMI4CPP_UNITDEFINITIONS_HPP
+#define FMI4CPP_UNITDEFINITIONS_HPP
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace fmi4cpp::fmi2
+{
+
+struct base_unit
+{
+    std::optional<int> kg = 0;
+    std::optional<int> m = 0;
+    std::optional<int> s = 0;
+    std::optional<int> A = 0;
+    std::optional<int> K = 0;
+    std::optional<int> mol = 0;
+    std::optional<int> cd = 0;
+    std::optional<int> rad = 0;
+    std::optional<double> factor = 1.0;
+    std::optional<double> offset = 0.0;
+};
+
+struct display_unit
+{
+    std::string name;
+    double factor = 1.0;
+    double offset = 0.0;
+};
+
+struct unit
+{
+    std::string name;
+    base_unit base_unit;
+    std::vector<display_unit> display_units;
+};
+
+struct unit_definitions
+{
+    std::vector<unit> units;
+};
+
+} // namespace fmi4cpp::fmi2
+
+#endif // FMI4CPP_UNITDEFINITIONS_HPP
